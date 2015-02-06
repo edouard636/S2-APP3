9ffdc8e86c52fdc436eec1193f504355434bd056
edouard636 <edouard636.t@gmail.com>
2015-02-06 18:19:59 -0500

    Create facture.h

---
+++ facture.h
@@ -0,0 +1,24 @@
+//!  Definition de classes
+/**
+    \file  facture.h
+    \author xxxxx
+    \date AAAA-MM-JJ
+    \version 1.00
+  
+    A developper pour menufact02    
+*/
+
+#ifndef FACTURE_H
+#define FACTURE_H
+
+/// Determine une Facture (a developper pour menufact02) ...
+/** \class Facture
+  A developper ...
+*/
+
+class Facture
+{
+// A developper ...
+};
+
+#endif //FACTURE_H
